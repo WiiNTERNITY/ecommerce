9025208b94636630e4196c7b2cdf9b156e07710a
wiinternity <hammani_mounir@outlook.fr>
2023-01-23 21:18:55 +0100

    ecommerce

--- src/components/BaseLayout/components/Navbar/index.js
+++ src/components/BaseLayout/components/Navbar/index.js
@@ -1,22 +1,35 @@
 // Dependencies
-import React from 'react';
-import { Icon } from 'react-materialize';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { Icon } from "react-materialize";
+import { NavLink } from "react-router-dom";
 // Internals
-import './index.css';
-
+import "./index.css";
 
 const Navbar = () => (
   <nav className="navbar">
     <div className="nav-links">
       <ul>
-        <li><NavLink activeClassName="selected" className="nav-link" exact to="/">Home</NavLink></li>
-        <li><NavLink activeClassName="selected" className="nav-link" to="/women">Women</NavLink></li>
-        <li><NavLink activeClassName="selected" className="nav-link" to="/men">Men</NavLink></li>
+        <li>
+          <NavLink activeClassName="selected" className="nav-link" exact to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="selected" className="nav-link" to="/women">
+            Women
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="selected" className="nav-link" to="/men">
+            Men
+          </NavLink>
+        </li>
       </ul>
     </div>
     <div className="shopping-cart">
-      <NavLink to="/cart"><Icon medium>shopping_cart</Icon></NavLink>
+      <NavLink to="/cart">
+        <Icon medium>shopping_cart</Icon>
+      </NavLink>
     </div>
   </nav>
 );
